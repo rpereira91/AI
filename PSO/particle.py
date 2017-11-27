1ac8efeac2a84b447afb2e2b23966659bdf3bd9f
rpereira91 <boveah@gmail.com>
2017-11-27 18:45:10 -0500

    Added comments

--- PSO/particle.py
+++ PSO/particle.py
@@ -24,19 +24,17 @@ class Particle:
         return self.bestFit
     def setX(self,x):
         self.x = x[:]
+    #gets the fitness of the x values
     def SolveRast(self):
         fit = 10*self.d
         for i in range(1,self.d):
             fit += self.x[i]**2 - (10*math.cos(2*math.pi*self.x[i]))
         return fit
+    #Update the best fitness of the particle
     def UpdateFitness(self):
         fit = self.SolveRast()
         if fit < self.bestFit:
             self.bestFit = fit
         self.f = fit
-    def BadFit(cls):
-        p = Particle(d)
-        p.bestFit = 1000
-        return cls(p)
 
         
