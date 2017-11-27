346fe7e23468dfdc0d4f680d370e28d54c636c76
rpereira91 <boveah@gmail.com>
2017-11-27 18:41:07 -0500

    Added get best fitness and sudo update velocity

--- PSO/particle.py
+++ PSO/particle.py
@@ -9,11 +9,10 @@ import math
 
 class Particle:
     def __init__(self,d):
-        self.x = self.GenXValues(d)
-        self.f = self.SolveRast(self.x,d)
+        self.d = d
+        self.x = [random.uniform(-5.12,5.12) for i in range(self.d)]
+        self.f = self.SolveRast()
         self.bestFit = self.f
-    def GenXValues(self,d):
-        return [random.uniform(-5.12,5.12) for i in range(d)]
     #Getter defs for the I and J location variables 
     def getX(self):
         return self.x
@@ -23,11 +22,21 @@ class Particle:
         return self.v
     def getBestFit(self):
         return self.bestFit
-    def setF(self,f):
-        self.f = f
-    def SolveRast(self,x,d):
-        fit = 10*d
-        for i in range(1,d):
-            fit += x[i]**2 - (10*math.cos(2*math.pi*x[i]))
+    def setX(self,x):
+        self.x = x[:]
+    def SolveRast(self):
+        fit = 10*self.d
+        for i in range(1,self.d):
+            fit += self.x[i]**2 - (10*math.cos(2*math.pi*self.x[i]))
         return fit
+    def UpdateFitness(self):
+        fit = self.SolveRast()
+        if fit < self.bestFit:
+            self.bestFit = fit
+        self.f = fit
+    def BadFit(cls):
+        p = Particle(d)
+        p.bestFit = 1000
+        return cls(p)
+
         
--- PSO/pso.py
+++ PSO/pso.py
@@ -2,16 +2,42 @@ from particle import Particle
  
 SwarmSize = 3
 Swarm = []
+d = 3
+#the best fit is a particle
+GlobalFit = Particle(d)
+#create a swarm with dimension D
 def InitilizeSwarm(d):
     for i in range(SwarmSize):
         p = Particle(d)
         Swarm.append(p)
+def UpdateVelocity():
+    for i in range(SwarmSize):
+        x = Swarm[i].getX()
+        for j in range(len(x)):
+            x[j] -= 0.1
+        Swarm[i].setX(x)
+        Swarm[i].UpdateFitness()
+#get the best fitness for the entire swarm
+def GetBestFit():
+    global GlobalFit
+    bf = GlobalFit
+    for i in range(SwarmSize):
+        if Swarm[i].getBestFit() < bf.getBestFit():
+            bf = Swarm[i]
+    return bf 
+#prints out the entire swarm
 def DisplaySwarm():
     for i in range(SwarmSize):
         print("Particle: " + str(Swarm[i].getX()) + "Best Fit: "+str(Swarm[i].getBestFit()))
 
 
 
-
-InitilizeSwarm(3)
-DisplaySwarm()
+InitilizeSwarm(d)
+#set the best fit to a high number
+GlobalFit.bestFit = 10000
+for i in range(2):
+    print("Pass by #: " + str(i+1))
+    DisplaySwarm()
+    GlobalFit = GetBestFit()
+    print("\nGlobal Best: " + str(GlobalFit.getX()))
+    UpdateVelocity()
