043674ae261d148e12b8e3990d8f43ac29079f0c
rpereira91 <boveah@gmail.com>
2017-11-28 20:14:21 -0500

    Velocity updating not working, forumila works, positioning maybe?

--- PSO/pso.py
+++ PSO/pso.py
@@ -13,43 +13,47 @@ class PSO:
         self.Swarm = []
         self.SwarmSize = ss
         self.epoch = epoch
-        #the best fit is a particle
-        self.GlobalFit = Particle(dimension)
-        self.GlobalFit.bestFit = 10000
+
 
     def SolvePso(self): 
-        InitilizeSwarm(self.d)
+        self.InitilizeSwarm()
+        #the best fit is a particle
+        GlobalFit = Particle(self.d)
+        GlobalFit.bestFit = 10000
         #set the best fit to a high number
         for i in range(self.epoch):
             print("\nPass by #: " + str(i+1))
             #DisplaySwarm()
-            self.GetBestFit()
+            
             print("Global Best: " + str(GlobalFit.bestFit))
-            UpdateVelocity(d,w,c1,c2)
+            self.UpdateVelocity(self.d,self.w,self.c1,self.c2,GlobalFit)
+            GlobalFit = self.GetBestFit(GlobalFit)
+
     def InitilizeSwarm(self):
         for i in range(self.SwarmSize):
             p = Particle(self.d)
-            self.Swarm.append(self.p)
-    def UpdateVelocity(self):
+            self.Swarm.append(p)
+    def UpdateVelocity(self,d,w,c1,c2,gb):
         for i in range(self.SwarmSize):        
-            for j in range(self.d):
+            for j in range(d):
                 r1 = random.random()  
                 r2 = random.random()
                 #calculate the new velocity 
-                self.Swarm[i].v[j] = ((w*self.Swarm[i].v[j])+(c1*r1*(self.Swarm[i].bestX[j]-self.Swarm[i].x[j]))+(c2*r2*(GlobalFit.x[j] - self.Swarm[i].x[j]))) 
+                self.Swarm[i].v[j] = ((w*self.Swarm[i].v[j])+(c1*r1*(self.Swarm[i].bestX[j]-self.Swarm[i].x[j]))+(c2*r2*(gb.x[j] - self.Swarm[i].x[j]))) 
                 #if the velocity goes past the bounds of the current x min and max, it will loop back around
                 if self.Swarm[i].v[j] < -5.12:
                     self.Swarm[i].v[j] = 5.12
                 elif self.Swarm[i].v[j] > 5.12:
                     self.Swarm[i].v[j] = -5.12
-            for j in range(self.d):
+            for j in range(d):
                 self.Swarm[i].x[j] += self.Swarm[i].v[j]
             self.Swarm[i].UpdateFitness()
     #get the best fitness for the entire swarm
-    def GetBestFit(self):
+    def GetBestFit(self,gb):
         for i in range(self.SwarmSize):
-            if self.Swarm[i].bestFit < self.GlobalFit.bestFit:
-                self.GlobalFit = self.Swarm[i]
+            if self.Swarm[i].bestFit < gb.bestFit:
+                gb = self.Swarm[i]
+        return gb
     #prints out the entire swarm
     def DisplaySwarm(self):
         for i in range(self.SwarmSize):
---
+++ PSO/runner.py
@@ -0,0 +1,3 @@
+from pso import PSO
+p1 = PSO(30,0.729844,1.496180,1.496180,40,10)
+p1.SolvePso()
