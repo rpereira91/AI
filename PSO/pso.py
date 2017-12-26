cceb3e88b868a1c95ba38ad373500af5d4b2b9d2
Unknown <boveah@gmail.com>
2017-12-25 20:10:09 -0500

    Finished PSO

--- PSO/pso.py
+++ PSO/pso.py
@@ -24,18 +24,20 @@ class PSO:
         self.epoch = epoch
 
 
-    def SolvePso(self,fn): 
+    def SolvePso(self,fn="NONE"): 
         self.InitilizeSwarm()
         #the best fit is a particle
         GlobalFit = Particle(self.d)
         GlobalFit.bestFit = 10000
         #set the best fit to a high number
-        f = open(fn,'w')
+        #f = open(fn,'w')
         for i in range(self.epoch):
             self.UpdateVelocity(self.d,self.w,self.c1,self.c2,GlobalFit)
             GlobalFit = self.GetBestFit(GlobalFit)
-            f.write(str(GlobalFit.bestFit)+"\n")
-        f.close()
+            #Only used for creating the test file
+            # f.write(str(GlobalFit.bestFit)+"\n")
+        return GlobalFit.bestFit
+        #f.close()
 
     def InitilizeSwarm(self):
         for i in range(self.SwarmSize):
@@ -51,13 +53,10 @@ class PSO:
                 r2 = random.random()
                 #calculate the new velocity 
                 self.Swarm[i].v[j] = ((w*self.Swarm[i].v[j])+(c1*r1*(self.Swarm[i].bestX[j]-self.Swarm[i].x[j]))+(c2*r2*(gb.x[j] - self.Swarm[i].x[j]))) 
-                #Update the x values
-                self.Swarm[i].x[j] += self.Swarm[i].v[j]
-                #Clamping the x values doesn't seem to work but here's were I would try to clamp
-                # if self.Swarm[i].x[j] > 5.12:
-                #     self.Swarm[i].x[j] = 5.12
-                # elif self.Swarm[i].x[j] < -5.12:
-                #     self.Swarm[i].x[j] = -5.12
+                #Update the x values only if it's inside the range of -5.12 and 5.12
+                if self.Swarm[i].x[j] + self.Swarm[i].v[j] >= -5.12 and self.Swarm[i].x[j] + self.Swarm[i].v[j] <= 5.12:
+                    self.Swarm[i].x[j] += self.Swarm[i].v[j]
+
                 #get the new fitness for the current particle
                 self.Swarm[i].UpdateFitness()
     #get the best fitness for the entire swarm
--- PSO/runner.py
+++ PSO/runner.py
@@ -5,24 +5,49 @@
 #############################
 
 from pso import PSO
-p1 = PSO(30,0.729844,1.496180,1.496180,200,5000)
-p2 = PSO(30,0.4,1.2,1.2,200,5000)
-p3 = PSO(30,1,2,2,200,5000)
-p4 = PSO(30,-1,2,2,200,5000)
-r1 = PSO(30,0.729844,1.496180,1.496180,200,5000)
+def CreateTestFiles():
+    p1 = PSO(30,0.729844,1.496180,1.496180,200,5000)
+    p2 = PSO(30,0.4,1.2,1.2,200,5000)
+    p3 = PSO(30,1,2,2,200,5000)
+    p4 = PSO(30,-1,2,2,200,5000)
+    r1 = PSO(30,0.729844,1.496180,1.496180,200,5000)
 
-for i in range(1):
-    # f1 = "Test\p1_" + str(i+1) + ".txt"
-    # f2 = "Test\p2_" + str(i+1) + ".txt"
-    # f3 = "Test\p3_" + str(i+1) + ".txt"
-    # f4 = "Test\p4_" + str(i+1) + ".txt"
-    # p1.SolvePso(f1)
-    # p2.SolvePso(f2)
-    # p3.SolvePso(f3)
-    # p4.SolvePso(f4)
-    # p1 = PSO(30,0.729844,1.496180,1.496180,200,5000)
-    # p2 = PSO(30,0.4,1.2,1.2,200,5000)
-    # p3 = PSO(30,1,2,2,200,5000)
-    # p4 = PSO(30,-1,2,2,200,5000)
-    rand = "Test\r1_" + str(i+1) + ".txt"
-    r1.RandomSearch(rand)
+    for i in range(1):
+        f1 = "Test\p1_" + str(i+1) + ".txt"
+        f2 = "Test\p2_" + str(i+1) + ".txt"
+        f3 = "Test\p3_" + str(i+1) + ".txt"
+        f4 = "Test\p4_" + str(i+1) + ".txt"
+        p1.SolvePso(f1)
+        p2.SolvePso(f2)
+        p3.SolvePso(f3)
+        p4.SolvePso(f4)
+        p1 = PSO(30,0.729844,1.496180,1.496180,200,5000)
+        p2 = PSO(30,0.4,1.2,1.2,200,5000)
+        p3 = PSO(30,1,2,2,200,5000)
+        p4 = PSO(30,-1,2,2,200,5000)
+        rand = "Test\r1_" + str(i+1) + ".txt"
+        r1.RandomSearch(rand)
+def CreateResults(x):
+    p1 = PSO(30,0.729844,1.496180,1.496180,200,5000)
+    p2 = PSO(30,0.4,1.2,1.2,200,5000)
+    p3 = PSO(30,1,2,2,200,5000)
+    p4 = PSO(30,-1,2,2,200,5000)
+    r1 = PSO(30,0.729844,1.496180,1.496180,200,5000)
+    pa1 = []
+    pa2 = []
+    pa3 = []
+    pa4 = []
+    for i in range(x):
+        print("Pass by",i)
+        pa1.append(p1.SolvePso())
+        pa2.append(p2.SolvePso())
+        pa3.append(p3.SolvePso())
+        pa4.append(p4.SolvePso())       
+    f = open("fn",'w')
+    f.write(str(sum(pa1)/len(pa1)))
+    f.write(str(sum(pa2)/len(pa2)))
+    f.write(str(sum(pa3)/len(pa3)))
+    f.write(str(sum(pa4)/len(pa4)))
+    f.close()
+
+CreateResults(2)
